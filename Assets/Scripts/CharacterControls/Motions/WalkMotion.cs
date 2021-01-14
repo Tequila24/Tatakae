dff73169be7ba2f20703f6fa4253898c44de08af
Tequila <1>
2021-01-14 15:49:11 +0300

    Leave walking as is, move to grappling

--- Assets/Scripts/CharacterControls/Motions/WalkMotion.cs
+++ Assets/Scripts/CharacterControls/Motions/WalkMotion.cs
@@ -33,7 +33,7 @@ namespace CharControl
 
             _charBody.velocity = Vector3.zero;
             _charBody.angularVelocity = Vector3.zero;
-            _charBody.rotation = Quaternion.Euler(0, _inputs.mousePositionX, 0);
+            //_charBody.rotation = Quaternion.Euler(0, _inputs.mousePositionX, 0);
 
 
             _currentSurface = SurfaceController.GetSurface( _charBody.transform.position, 
@@ -51,26 +51,65 @@ namespace CharControl
             _currentSurface = SurfaceController.GetSurface( _charBody.transform.position, 
                                                             Physics.gravity, 
                                                             _charCollider.bounds.size.y, 
-                                                            _charBody.transform);
+                                                            _charBody.transform );
 
             float currentSurfaceIncline = Vector3.Angle(Vector3.up, _currentSurface.contactPointNormal);
 
             if (currentSurfaceIncline < 30) {
-                // walk or stairs
-                
+
+                if ( /*not stairs*/ true) {     // how to distinguish between stairs and any other surface?
+                    
+                    // walking on surface
+                    Vector3 step = (_charBody.transform.forward * (_inputs.forward - _inputs.backward) +
+                                    _charBody.transform.right * (_inputs.right - _inputs.left)).normalized *
+                                    (_inputs.shift != 0 ? 0.15f : 0.05f);
+
+                    _velocity = Vector3.Lerp(_velocity, step, 0.5f);
+
+
+                    Vector3 heightAdjust = new Vector3(0, (_currentSurface.contactPoint.y + _charCollider.bounds.extents.y * 1.3f) - _charBody.transform.position.y, 0) * 0.2f;
+
+
+                    _charBody.MovePosition( _charBody.transform.position + 
+                                            heightAdjust + 
+                                            _currentSurface.rotationToNormal * _velocity);
+
+
+
+                    // rotation
+                    Quaternion lookRotation = Quaternion.Euler(0, _inputs.mousePositionX, 0);           // rotation to mouse look
+
+                    _charBody.MoveRotation( Quaternion.RotateTowards(   _charBody.transform.rotation,
+                                                                        lookRotation,
+                                                                        5.0f ) );
+
+
+                } else {
+                    // walking on stairs
+                    // Do I Even Need This?!
+                }
+
             } else {
-                // sliding
-                _velocity = Vector3.Lerp(_velocity, Vector3.Project(Physics.gravity * Time.deltaTime, _currentSurface.downhillVector), 0.2f);
+
+                // sliding downhill
+                _velocity = Vector3.Lerp(_velocity, _currentSurface.downhillVector * 0.5f , 0.2f);
+
+                _charBody.MovePosition( _charBody.transform.position + 
+                                        _currentSurface.rotationToNormal * _velocity );
+
+                _charBody.MoveRotation( Quaternion.RotateTowards(   _charBody.transform.rotation,
+                                                                    Quaternion.LookRotation(_currentSurface.downhillVector, Vector3.up),
+                                                                    5.0f ) );
             }
 
 
 
-            _charBody.MovePosition(_charBody.transform.position + 
+            
                                    //rotationToElevation *           // Rotation to surface normal
-                                   _velocity
+                                   
                                    //_currentSurface.contactPointVelocity * Time.deltaTime + 
                                    //heightAdjust
-                                   );
+                                   
 
             /*
 
@@ -138,9 +177,10 @@ namespace CharControl
 
         public override Vector3 GetVelocity()
         {
-            return _velocity;
+            return _currentSurface.rotationToNormal * _velocity;
         }
 
+        /*
         private void UpdateSurfaces()
         {
             
@@ -150,6 +190,7 @@ namespace CharControl
                                                         _charCollider.bounds.size.y, 
                                                         _charBody.transform);
         }
+        */
 
     }
 }
